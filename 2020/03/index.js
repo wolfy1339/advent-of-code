6aaaf48b7bc0a7fa9c3440ebf0dd1fb39358b525
wolfy1339 <webmaster@wolfy1339.com>
2020-12-06 18:48:30 -0500

    Day 3: Starting coordinates are (0,0)

--- 2020/03/index.js
+++ 2020/03/index.js
@@ -4,11 +4,11 @@ import { resolve } from 'path';
 
 const map = splitInputLines(resolve('./data')).filter(Boolean).map(e=>e.split(''));
 
-let posX = 3;
+let posX = 0;
 let treeCount = 0;
 const maxPosX = map[0].length;
 
-for (let posY = 1; posY < map.length; posY++) {
+for (let posY = 0; posY < map.length; posY++) {
   if (map[posY][posX] === '#') treeCount += 1;
   posX += 3;
   if (posX > maxPosX) posX = 3;
