13557a28f0bdd2c82b84137c0527d23122ed8ece
wolfy1339 <webmaster@wolfy1339.com>
2020-12-06 18:49:57 -0500

    Day 3: posX is a modulo of the map width

--- 2020/03/index.js
+++ 2020/03/index.js
@@ -4,14 +4,14 @@ import { resolve } from 'path';
 
 const map = splitInputLines(resolve('./data')).filter(Boolean).map(e=>e.split(''));
 
+// Part 1
 let posX = 0;
 let treeCount = 0;
 const maxPosX = map[0].length;
 
 for (let posY = 0; posY < map.length; posY++) {
   if (map[posY][posX] === '#') treeCount += 1;
-  posX += 3;
-  if (posX > maxPosX) posX = 3;
+  posX = (posX + 3) % maxPosX;
 }
 
 console.log(`You will encounter ${treeCount} trees`);
